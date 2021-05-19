eb2fc0e3b8a13c161a38d19c8ccba885b3a01860
seegee911 <seegee911@gmail.com>
2021-05-18 19:46:55 -0700

    cleaned up git config, added git_ignore funct

--- dotfiles-plus.plugin.zsh
+++ dotfiles-plus.plugin.zsh
@@ -2,14 +2,13 @@
 # GitHub: https://github.com/see-gee/dotfiles-plus
 
 # Variables
-DOTFILES_DIR=$HOME/.dotfiles
-DOTFILES_URL=""
+DOTFILES_GIT_DIR=$DOTFILES_GIT_DIR:-$HOME/.dotfiles
+DOTFILES_GIT_URL=""
 
 # Functions
-_dotfiles_staged_files() {
-  local diffList=$(git dotfiles--no-pager diff --name-only)
-  [ ! -z ${diffList// } ] && return
-  false
+_dotfiles_git_staged() {
+  local difflist=$(git dotfiles--no-pager diff --name-only)
+  [[ ! -z ${difflist// } ]] && return 0 || return 1
 }
 
 _dotfiles_git_alias() {
@@ -19,47 +18,79 @@ _dotfiles_git_alias() {
     return 0
   else
     echo "Git Alias for 'dotfiles' was not found. Creating..."
-    git config --global alias.dotfiles '!git --git-dir=$DOTFILES_DIR --work-tree=$HOME' 
+    git config --global alias.dotfiles '!git --git-dir="$DOTFILES_DIR" --work-tree="$HOME"' 
+  fi
+}
+
+_dotfiles_git_ignore() {
+  _dotfiles_git_ignore_add() {
+    echo "Adding a few good ideas to $HOME/.gitignore file"
+    ignore_list=(
+      ".ssh"          
+    )
+    echo "'# Added by dotfiles-plus plugin on '$(date)" >> $HOME/.gitignore
+    for i in ${ignore_list}; do
+      echo "Adding $i"
+      echo "$i\n" >> $HOME/.gitignore    
+    done
+  }
+  if [[ -f $HOME/.gitignore ]]; then
+    echo "Existing .gitignore file found:"
+    cat $HOME/.gitignore
+    echo "Would you like to create a backup of your existing .gitignore file \nand add some sensibile sauce to it? (Y/n)"
+    read -qs "ans?"
+    if [[ $ans = y ]]; then    
+      backup=$HOME/.gitignore.pre.dotfiles-plus
+      \cp -fp $HOME/.gitignore $backup
+      echo "$backup created"
+      _dotfiles_git_ignore_add      
+    else
+      echo "Alright. I'll just ignore your .gitignore file then."
+      return 0
+    fi    
+  else
+    _dotfiles_git_ignore_add
   fi
 }
 
 _dotfiles_git_config() {
-  cmd="git dotfiles config --local"
-  typeset -A opts=(
-    showUntrackedFiles "no"
-    submodule.recurse "true"
-    submodule.fetchjobs "8"
+  echo "Configuring Git:"  
+  threadcount:-$(getconf _NPROCESSORS_ONLN)# Get CPU thread count
+  [[  $threadcount > 8 ]] && echo "$threadcount threads. Nice CPU ;)"; threadcount=8
+  typeset -A git_config_opts=(
+    status.showuntrackedfiles no
+    submodule.recurse true
+    submodule.fetchjobs $cpu_threads
   )  
-  
-  for key in ${(@kv)opts}; do
-    if [[ $($cmd" --get "$key) ]]; then
-      echo "Option [$key] already set"
-      return 0
+  for key val in ${(@kv)git_config_opts}; do
+    if [[ "$(git dotfiles config --get $key)" ]]; then
+      echo "$key already set to [$val], no changes made"      
     else
-      echo "Setting [$key] to $val"
-      $cmd $key $val
+      echo "Setting $key to [$val]"
+      git dotfiles config $key $val
     fi    
-  done
+  done  
 }
 
-_dotfiles_init_repo() {
+_dotfiles_git_init() {
   if [[ ! -d $DOTFILES_DIR ]]; then
     mkdir $DOTFILES_DIR
-    git  --git-dir=$DOTFILES_DIR --work-tree=$HOME init --bare
+    git  --git-dir="$DOTFILES_DIR" --work-tree="$HOME" init --bare
     _dotfiles_git_config
+    return 0
   else
     echo "$DOTFILES_DIR directory already exists. 
     Move or delete to create a new repository"
+    return 1
   fi
 }
 
 _dotfiles_help() {
   echo "Dotfiles Plus - Oh My Zsh Plugin\n
-    The dotfiles command is an easy way to keep your dotfiles organized and
-    synchronized using a git repository.
+    The dotfiles command is a helper script to easily keep your dotfiles 
+    organized and synchronized using a git repository.\n
   Usage:
-    dotfiles <command> <options>
-  
+    dotfiles <command> <options>\n  
   Commands:
     add               Add a file to your dotfiles repo
     commit            Commit changes to dotfiles
@@ -78,12 +109,16 @@ _dotfiles_help() {
 }
 
 dotfiles() {
-  if [[ -z "$@" ]] || [[ $1 = ("help" || "--help") ]]; then
+  if [[ -z "$@" ]] || [[ $1 = "help" ]] || [[ $1 = "--help" ]] || [[ $1 = "-h" ]]
+  then
     _dotfiles_help
   elif [[ $1 = "download" ]]; then
     git dotfiles pull --recurse-submodules -j8
   elif [[ $1 = "upload" ]]; then    
     git dotfiles push
+  elif [[ $1 = "commit" ]]; then
+    _dotfiles_git_staged
+    _dotfiles_git_commit
   else
     git dotfiles "$@"    
   fi
