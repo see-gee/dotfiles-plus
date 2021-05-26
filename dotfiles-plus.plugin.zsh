f6a6b2efd526b29b47d35bf61763b52b114cb54d
seegee911 <seegee911@gmail.com>
2021-05-25 20:08:37 -0700

    reup

--- dotfiles-plus.plugin.zsh
+++ dotfiles-plus.plugin.zsh
@@ -2,8 +2,8 @@
 # GitHub: https://github.com/see-gee/dotfiles-plus
 
 # Variables
-DOTFILES_GIT_DIR=${DOTFILES_GIT_DIR:=$HOME/.dotfiles}
-DOTFILES_GIT_REPO=
+DOTFILES_DIR=${DOTFILES_DIR:=$HOME/.dotfiles}
+DOTFILES_URL=${DOTFILES_URL:="$(git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" remote get-url origin)"}
 
 # Alias to call git with proper path options. 
 # Using a git alias is preferred over full command line substitution.
@@ -11,25 +11,24 @@ DOTFILES_GIT_REPO=
 # if not, command substitution will be used. You can create the git alias
 # by calling the _dotfiles_git_alias function.
 if [[ $(git config --global --get alias.dotfiles) ]]; then
-  alias _dotfiles_git_cmd="git dotfiles"
+  alias _dotfiles_git="git dotfiles"
 else
-  alias _dotfiles_git_cmd="git --git-dir=$DOTFILES_GIT_DIR --work-tree=$HOME"
+  alias _dotfiles_git="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 fi
 
 # Autoload Functions can be found in the same folder as this file.
 # Filenames begin with an underscore (_) and match the name of the 
-# function they call. Using the autoload feature of zsh helps reduce 
-# unnecessary system resource usage.
+# function they call. Using the function autoload feature of zsh helps
+# reduce unnecessary system resource usage.
 # autoload -Uz ${0:h}/_*
-
 
 dotfiles() {
   if [[ -z "$@" ]] || [[ $1 = "help" ]] || [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then
     _dotfiles_help
   elif [[ $1 = "download" ]]; then
-    _dotfiles_git_cmd pull --recurse-submodules -j8t
+    _dotfiles_git pull --recurse-submodules -j8t
   elif [[ $1 = "upload" ]]; then    
-    _dotfiles_git_cmd push
+    _dotfiles_git push
   elif [[ $1 = "commit" ]]; then
     _dotfiles_git_staged
     _dotfiles_git_commit
@@ -37,10 +36,11 @@ dotfiles() {
     _dotfiles_git_submodule
   elif [[ $1 = "setup" ]]; then
     _dotfiles_git_alias
+    _dotfiles_ignore
     _dotfiles_git_config
-    _dotfiles_git_ignore_add
+    _dotfiles_ignore
   else
-     _dotfiles_git_cmd "$@"    
+     _dotfiles_git "$@"    
   fi
 }
 
